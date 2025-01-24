284d760c04e71b63a1f3eef926c183e23cea208b
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-01-24 15:13:32 +0530

    Create Calculates the median of a given array of integers.java

---
+++ Arrays/Ccalculates the median of a given array of integers.java
@@ -0,0 +1,12 @@
+Problem Statement:
+Write a program that calculates the median of a given array of integers. The program should prompt the user to input the size of the array followed by the array elements.
+
+The median is defined as the middle element of a sorted array if the array has an odd number of elements.
+If the array has an even number of elements, the median is the average of the two middle elements.
+
+Input Format:
+The first line of input consists of the array size.
+The second line of input consists of an array of elements, separated by space.
+
+CODE:
+
