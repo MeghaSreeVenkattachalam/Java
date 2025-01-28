4c6c4d2abd76f13852d7648ced720c78884af107
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-01-28 23:28:30 +0530

    Create Print the Consonents.java

---
+++ String/Print the Consonents.java
@@ -0,0 +1,33 @@
+Find only consonent characters or remove vowels in a string?
+
+CODE:
+
+import java.util.*;
+class Consonent_Characters{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string input: ");
+		String str = sc.nextLine();
+		
+		//converted them into toCharArray
+		char ch[] =str.toCharArray();
+		for(int i=0;i<str.length();i++){
+			
+			//if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+			
+			if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'||ch[i]=='A'||ch[i]=='E'||ch[i]=='I'||ch[i]=='O'||ch[i]=='U'){
+				continue;
+			}
+			else{
+				System.out.println("The consonents in the given input is "+ch[i]);
+			}
+			//without else
+				//give System.out.print(str.charAt(i));
+		}
+		
+	}
+}
+
+OUTPUT:
+Apple is a healthy fruit
+The consonents in the given input is ppl s hlthy frt
