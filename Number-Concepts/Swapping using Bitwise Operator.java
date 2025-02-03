eb8fa5883c555ed5ae6652e67b26ea037293774a
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-02-03 20:05:11 +0530

    Create Swapping using Bitwise Operator.java

---
+++ Number-Concepts/Swapping using Bitwise Operator.java
@@ -0,0 +1,29 @@
+Swapping elements using bitwise operator.
+
+import java.util.*;
+class Swap_Using_Bitwise{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		//this is only working for 5 and 7
+		/*
+		int a1=a|b;
+		int b1=a&b;
+		*/
+		
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+OUTPUT:
+2
+5
+
+5
+2
