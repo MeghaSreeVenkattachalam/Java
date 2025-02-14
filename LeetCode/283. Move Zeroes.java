3221b8ff9dce75b0ba4f88444b6f07c410cbff6d
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-02-14 23:25:58 +0530

    Update 283. Move Zeroes.java

--- LeetCode/283. Move Zeroes.java
+++ LeetCode/283. Move Zeroes.java
@@ -39,3 +39,37 @@ Runtime
 ms
 Beats
 5.02%
+
+
+/*
+
+
+
+import java.util.*;
+class Move_Zeros{
+    public static void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j++] = nums[i];
+            }
+        }
+        while(j<nums.length){
+            nums[j++] = 0;
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int nums[] = new int[n];
+		for(int i=0;i<n;i++){
+			nums[i] = sc.nextInt();
+		}
+		moveZeroes(nums);
+	}
+}
+
+*/
