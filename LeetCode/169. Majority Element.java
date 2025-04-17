e7c315518e39aba651e2fca0f89ed52c75a8de6b
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-04-17 22:09:23 +0530

    Update 169. Majority Element.java

--- LeetCode/169. Majority Element.java
+++ LeetCode/169. Majority Element.java
@@ -30,3 +30,66 @@ Runtime
 ms
 Beats
 57.63%
+
+ //ANOTHER APPROACH: 
+
+ CODE:
+
+ class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0, count = 0;
+
+        // Phase 1: Find a candidate
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        // Phase 2: Verify the candidate
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return count > nums.length / 2 ? candidate : -1; // -1 if no majority element
+    
+        //Arrays.sort(nums);
+        //return nums[nums.length/2];
+    }
+}
+
+OUTPUT:
+
+Runtime
+1
+ms
+Beats
+99.88%
+
+ //ANOTHER APPROACH:
+
+ CODE:
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new TreeMap<>();
+        int len = nums.length/2;
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+            if(map.get(i)>len) return i;
+        }
+        return -1;
+    }
+}
+
+OUTPUT:
+
+Runtime
+18
+ms
+Beats
+12.82
