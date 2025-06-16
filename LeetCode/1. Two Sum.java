f06098ea53e8b293e90130697ceb3dfac2cafeb8
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-06-16 22:10:59 +0530

    Update 1. Two Sum.java

--- LeetCode/1. Two Sum.java
+++ LeetCode/1. Two Sum.java
@@ -31,6 +31,29 @@ CODE:
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int val = target-nums[i];
+            if(map.containsKey(val)){
+                return new int[]{map.get(val),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
+
+OUTPUT:
+Runtime
+2
+ms
+Beats
+98.89%
+
+
+//ANOTHER APPROACH:
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         int[] arr= new int[2];
         int j=0;
